6c2204a861ca3dd9182c49e110b0d38030c0572f
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-10 22:03:40 -0500

    Login page 3

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_codigo5_alerta/ui/general/colors.dart';
 import 'package:flutter_codigo5_alerta/ui/widgets/general_widgets.dart';
 import 'package:flutter_codigo5_alerta/ui/widgets/input_textfield_password_widget.dart';
 import 'package:flutter_codigo5_alerta/ui/widgets/input_textfield_widget.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -21,6 +22,12 @@ class _LoginPageState extends State<LoginPage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
+              divider20(),
+              SvgPicture.asset(
+                'assets/images/imagen2.svg',
+                height: 120,
+              ),
+              divider20(),
               const Text(
                 "Bienvenido",
                 style: TextStyle(
@@ -62,23 +69,22 @@ class _LoginPageState extends State<LoginPage> {
               divider20(),
               RichText(
                 text: TextSpan(
-                  text: "¿Aún no estás registrado? ",
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white54,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: " Regístrate",
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff5468FF),
-                      ),
+                    text: "¿Aún no estás registrado? ",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white54,
                     ),
-                  ]
-                ),
+                    children: [
+                      TextSpan(
+                        text: " Regístrate",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff5468FF),
+                        ),
+                      ),
+                    ]),
               ),
             ],
           ),
