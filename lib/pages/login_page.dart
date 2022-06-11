aae8b609e80127f1e98fc81c2c725deab782112a
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-10 21:56:00 -0500

    Login page 2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_alerta/ui/general/colors.dart';
+import 'package:flutter_codigo5_alerta/ui/widgets/general_widgets.dart';
 import 'package:flutter_codigo5_alerta/ui/widgets/input_textfield_password_widget.dart';
 import 'package:flutter_codigo5_alerta/ui/widgets/input_textfield_widget.dart';
 
@@ -16,29 +17,71 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: kBrandPrimaryColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            Text(
-              "Bienvenido",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28.0,
-                fontWeight: FontWeight.w600,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                "Bienvenido",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            Text(
-              "Por favor, ingresa tus credenciales",
-              style: TextStyle(
-                color: Colors.white54,
+              const Text(
+                "Por favor, ingresa tus credenciales",
+                style: TextStyle(
+                  color: Colors.white54,
+                ),
               ),
-            ),
-            InputTextFieldWidget(
-              hintText: "Nro. DNI",
-              maxLength: 8,
-              textInputType: TextInputType.number,
-            ),
-            InputTextFieldPasswordWidget(),
-          ],
+              divider12(),
+              InputTextFieldWidget(
+                hintText: "Nro. DNI",
+                maxLength: 8,
+                textInputType: TextInputType.number,
+              ),
+              InputTextFieldPasswordWidget(),
+              divider20(),
+              SizedBox(
+                width: double.infinity,
+                height: 54.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff5468FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Iniciar Sesión",
+                  ),
+                ),
+              ),
+              divider20(),
+              RichText(
+                text: TextSpan(
+                  text: "¿Aún no estás registrado? ",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white54,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: " Regístrate",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xff5468FF),
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/ui/widgets/general_widgets.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+SizedBox divider12() => const SizedBox(height: 12.0);
+SizedBox divider20() => const SizedBox(height: 20.0);
+SizedBox divider30() => const SizedBox(height: 30.0);
+SizedBox divider40() => const SizedBox(height: 40.0);
