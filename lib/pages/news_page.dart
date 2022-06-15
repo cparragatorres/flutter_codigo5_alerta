a07d2adab419b47d67dd284d8f6f3b3895586460
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-14 22:18:39 -0500

    Home 5

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class NewsPage extends StatelessWidget {
@@ -6,6 +5,30 @@ class NewsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Noticias"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Image.network(
+                "http://alertahunter.herokuapp.com/media/Noticias/pexels-photo-11181151.jpeg",
+              ),
+              Image.network(
+                "http://alertahunter.herokuapp.com/media/Noticias/pexels-photo-12315780.jpeg",
+                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                  return Text("sdsdsd");
+                },
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
