975f5125173c0f0d0d0aba5692dfa7010d6b340e
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-15 19:44:44 -0500

    Noticias 5

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -39,53 +39,70 @@ class _NewsPageState extends State<NewsPage> {
         itemCount: news.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            margin:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+            margin: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 12.0,
+            ),
             width: double.infinity,
             height: 260,
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.network(
-                  news[index].imagen,
-                  fit: BoxFit.cover,
-                  errorBuilder: (BuildContext context, Object error,
-                      StackTrace? stackTrace) {
-                    return Image.network(
-                      "https://images.pexels.com/photos/11513528/pexels-photo-11513528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                      fit: BoxFit.cover,
-                    );
-                  },
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.center,
-                        colors: [
-                          Colors.black.withOpacity(0.7),
-                          Colors.transparent,
-                        ],
-                      )),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        news[index].titulo,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 15.0,
-                        ),
-                      ),
-                    ],
+            child:  ClipRRect(
+              borderRadius: BorderRadius.circular(14.0,),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    news[index].imagen,
+                    fit: BoxFit.cover,
+                    errorBuilder: (BuildContext context, Object error,
+                        StackTrace? stackTrace) {
+                      return Image.asset(
+                        "assets/images/imagex1.jpeg",
+                        fit: BoxFit.cover,
+                      );
+                    },
                   ),
-                ),
-              ],
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.center,
+                          colors: [
+                            Colors.black.withOpacity(0.7),
+                            Colors.transparent,
+                          ],
+                        )),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0, vertical: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          news[index].titulo,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        Text(
+                          news[index].fecha.toString().substring(0,10),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Colors.white54,
+                            fontSize: 13.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
