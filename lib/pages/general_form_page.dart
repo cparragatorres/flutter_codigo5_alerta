daba11e0aca648807186c1b2e737d7f2c5c36690
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-15 20:04:13 -0500

    Noticias 7

--- lib/pages/general_form_page.dart
+++ lib/pages/general_form_page.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_alerta/ui/general/colors.dart';
+import 'package:flutter_codigo5_alerta/ui/widgets/input_textfield_widget.dart';
 
 class GeneralFormPage extends StatefulWidget {
   const GeneralFormPage({Key? key}) : super(key: key);
@@ -9,8 +10,35 @@ class GeneralFormPage extends StatefulWidget {
 }
 
 class _GeneralFormPageState extends State<GeneralFormPage> {
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _linkController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: kBrandPrimaryColor,
+      appBar: AppBar(
+        backgroundColor: kBrandPrimaryColor,
+        title: Text("Form"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14.0),
+          child: Column(
+            children: [
+              InputTextFieldWidget(
+                hintText: "Título",
+                controller: _titleController,
+              ),
+              InputTextFieldWidget(
+                hintText: "Link",
+                controller: _linkController,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
