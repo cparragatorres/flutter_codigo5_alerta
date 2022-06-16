8d979816f2b0ea00c78ee48bd37864f98aea8507
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-15 19:22:28 -0500

    Noticias 1

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_alerta/models/news_model.dart';
 import 'package:flutter_codigo5_alerta/services/api_service.dart';
+import 'package:flutter_codigo5_alerta/ui/general/colors.dart';
 
 class NewsPage extends StatefulWidget {
   @override
@@ -8,8 +9,6 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-
-
   final APIService _apiService = APIService();
   List<NewsModel> news = [];
 
@@ -20,12 +19,10 @@ class _NewsPageState extends State<NewsPage> {
     getData();
   }
 
-  getData(){
+  getData() {
     _apiService.getNews().then((value) {
       news = value;
-      setState(() {
-
-      });
+      setState(() {});
     });
   }
 
@@ -33,12 +30,35 @@ class _NewsPageState extends State<NewsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: kBrandPrimaryColor,
         title: Text("Noticias"),
+        elevation: 0,
       ),
       body: ListView.builder(
         itemCount: news.length,
-        itemBuilder: (BuildContext context, int index){
-          return Text(news[index].titulo);
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+            width: double.infinity,
+            height: 260,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.network(
+                  news[index].imagen,
+                  fit: BoxFit.cover,
+                  errorBuilder: (BuildContext context, Object error,
+                      StackTrace? stackTrace) {
+                    return Image.network(
+                      "https://images.pexels.com/photos/11513528/pexels-photo-11513528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
