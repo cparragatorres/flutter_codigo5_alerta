34e8f35d21365f543ae28de8f67776562975d305
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-17 19:40:14 -0500

    Alerts 2

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_alerta/models/alert_model.dart';
 import 'package:flutter_codigo5_alerta/services/api_service.dart';
 import 'package:flutter_codigo5_alerta/ui/general/colors.dart';
 
@@ -10,27 +9,73 @@ class AlertPage extends StatefulWidget {
 }
 
 class _AlertPageState extends State<AlertPage> {
-
   final APIService _apiService = APIService();
+  List<AlertModel> alerts = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _apiService.getAlerts();
+    getData();
   }
 
-
+  getData() {
+    _apiService.getAlerts().then((value) {
+      alerts = value;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBrandPrimaryColor,
       appBar: AppBar(
         backgroundColor: kBrandPrimaryColor,
         title: Text("Alertas"),
-
+      ),
+      body: ListView.builder(
+        itemCount: alerts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: ListTile(
+              title: Text(
+                alerts[index].tipoIncidente.titulo,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14.0,
+                ),
+              ),
+              subtitle: Text(
+                "${alerts[index].ciudadanoNombre}",
+                style: const TextStyle(
+                  color: Colors.white70,
+                ),
+              ),
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    alerts[index].fecha,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                  Text(
+                    alerts[index].hora,
+                    style: const TextStyle(
+                      color: Colors.white54,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
-
