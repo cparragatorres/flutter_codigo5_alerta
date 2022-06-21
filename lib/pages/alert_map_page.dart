a2e35704e1b39abbe9d3b291918a0846f2cf78c3
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-20 22:18:25 -0500

    Google maps 3

--- lib/pages/alert_map_page.dart
+++ lib/pages/alert_map_page.dart
@@ -1,5 +1,9 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_alerta/utils/map_style.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AlertMapPage extends StatefulWidget {
   @override
@@ -7,10 +11,25 @@ class AlertMapPage extends StatefulWidget {
 }
 
 class _AlertMapPageState extends State<AlertMapPage> {
+
+  final CameraPosition _cameraPosition = const CameraPosition(
+    target: LatLng(-16.389939, -71.547106),
+    zoom: 15,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: GoogleMap(
+        initialCameraPosition: _cameraPosition,
+        compassEnabled: true,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: false,
+        mapType: MapType.normal,
+        onMapCreated: (GoogleMapController controller){
+          controller.setMapStyle(json.encode(mapStyle));
+        },
+      ),
     );
   }
 }
