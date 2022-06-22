ee4864174b9f653fbbabd41ba3257bc7c67d6918
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-06-21 20:22:28 -0500

    Google maps 10

--- lib/pages/alert_map_page.dart
+++ lib/pages/alert_map_page.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_alerta/models/alert_model.dart';
+import 'package:flutter_codigo5_alerta/ui/general/colors.dart';
 import 'package:flutter_codigo5_alerta/utils/map_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AlertMapPage extends StatefulWidget {
-
   List<AlertModel> alerts;
+
   AlertMapPage({required this.alerts});
 
   @override
@@ -42,7 +43,7 @@ class _AlertMapPageState extends State<AlertMapPage> {
   ];
 
   @override
-  initState(){
+  initState() {
     super.initState();
     // getMarkersLocation();
     getMarkerAlerts();
@@ -59,29 +60,86 @@ class _AlertMapPageState extends State<AlertMapPage> {
       _markers2.add(_marker);
       // _markers[_markerId] = _marker;
     });
-    setState((){});
+    setState(() {});
   }
 
-  getMarkerAlerts(){
+  getMarkerAlerts() {
     widget.alerts.forEach((element) {
       MarkerId _markerId = MarkerId(_markers2.length.toString());
       Marker _marker = Marker(
         markerId: _markerId,
         position: LatLng(element.latitud, element.longitud),
+        onTap: () {
+          showDetailAlert();
+        },
       );
       _markers2.add(_marker);
     });
 
-    setState((){});
-
+    setState(() {});
   }
 
-
+  showDetailAlert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: kBrandPrimaryColor,
+          contentPadding: EdgeInsets.zero,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Detalle de la alerta",
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+              Divider(
+                indent: 12.0,
+                endIndent: 12.0,
+                color: Colors.white30,
+              ),
+              Text(
+                "Secuestro",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Fecha: 12/12/2022",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    "Hora: 12:22:11",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: _cameraPosition,
@@ -94,31 +152,31 @@ class _AlertMapPageState extends State<AlertMapPage> {
         },
         // markers: _markers.values.toSet(),
         markers: _markers2,
-        onTap: (LatLng latLng) {
-          MarkerId _myMarkerId = MarkerId(_markers.length.toString());
-          Marker _myMarker = Marker(
-              markerId: _myMarkerId,
-              position: latLng,
-              infoWindow: InfoWindow(
-                title: "Hola",
-              ),
-              icon: BitmapDescriptor.defaultMarkerWithHue(
-                  BitmapDescriptor.hueAzure),
-              onTap: () {
-                print("MARKER");
-              },
-              draggable: true,
-              onDragStart: (LatLng position) {},
-              onDrag: (LatLng position) {
-                print("DRAGGGGGG");
-              },
-              onDragEnd: (LatLng position) {
-                print("DRAGGGGGG ENDDDDDDDDDDDDDDDDD");
-              });
-          _markers[_myMarkerId] = _myMarker;
-          _markers2.add(_myMarker);
-          setState(() {});
-        },
+        // onTap: (LatLng latLng) {
+        //   MarkerId _myMarkerId = MarkerId(_markers.length.toString());
+        //   Marker _myMarker = Marker(
+        //       markerId: _myMarkerId,
+        //       position: latLng,
+        //       infoWindow: InfoWindow(
+        //         title: "Hola",
+        //       ),
+        //       icon: BitmapDescriptor.defaultMarkerWithHue(
+        //           BitmapDescriptor.hueAzure),
+        //       onTap: () {
+        //         print("MARKER");
+        //       },
+        //       draggable: true,
+        //       onDragStart: (LatLng position) {},
+        //       onDrag: (LatLng position) {
+        //         print("DRAGGGGGG");
+        //       },
+        //       onDragEnd: (LatLng position) {
+        //         print("DRAGGGGGG ENDDDDDDDDDDDDDDDDD");
+        //       });
+        //   _markers[_myMarkerId] = _myMarker;
+        //   _markers2.add(_myMarker);
+        //   setState(() {});
+        // },
       ),
     );
   }
